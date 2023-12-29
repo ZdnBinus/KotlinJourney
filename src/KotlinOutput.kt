79b09d5ad3fbee4e3ea6332b6ea82139630e58a5
Zaidan <zaidan.aman@binus.ac.id>
2023-12-29 19:14:31 +0700

    KotlinOutput 122823

--- src/KotlinOutput.kt
+++ src/KotlinOutput.kt
@@ -1,7 +1,16 @@
-class KotlinOutput {
-
-}
-
 fun main(){
-    println("Hello world ");
+    //You can add as many println() functions as you want. Note that it will add a new line for each function
+    println("Hello world ")
+    println("i'm Learning Kotlin")
+    println("It's Awesome!")
+
+    //You can also print numbers, and perform mathematical calculations
+    println(3+3)
+
+    //There is also a print() function it does not insert a new line at the end of the output
+    print("Hello world ")
+    print("Are you ready ")
+    print("well that's how it goes")
+
+
 }
