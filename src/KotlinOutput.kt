a17c139fda3eb12736043d840ab92e38dc9c7a04
chingoo <zaidan.aman@binus.ac.id>
2025-01-06 23:53:53 +0700

    updated

--- src/KotlinOutput.kt
+++ src/KotlinOutput.kt
@@ -1,16 +1,8 @@
+class MineCraft{
+
+}
+
 fun main(){
-    //You can add as many println() functions as you want. Note that it will add a new line for each function
-    println("Hello world ")
-    println("i'm Learning Kotlin")
-    println("It's Awesome!")
-
-    //You can also print numbers, and perform mathematical calculations
-    println(3+3)
-
-    //There is also a print() function it does not insert a new line at the end of the output
-    print("Hello world ")
-    print("Are you ready ")
-    print("well that's how it goes")
 
 
 }
