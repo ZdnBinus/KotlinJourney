69fdd205e139c18325181560ca97f2a8d89648b2
chingoo <zaidan.aman@binus.ac.id>
2025-01-07 08:13:37 +0700

    updated

--- src/KotlinOutput.kt
+++ src/KotlinOutput.kt
@@ -1,8 +1,29 @@
-class MineCraft{
+class MineCraft(var name:String, var tipe:String, var hp:Int){
+
+    fun bicara(ngomong:String){
+        println("si $tipe berbicara $ngomong")
+    }
 
 }
 
 fun main(){
+
+    println("Masukan nama")
+    var name = readln().toString()
+    println("Masukan tipe")
+    var tipe = readln().toString()
+    println("Masukan max darah")
+    var hp = readln().toInt()
+    println("Bagaimana dia berbicara")
+    var bicara = readln().toString()
+
+    var human = MineCraft(name, tipe, hp)
+
+
+    println("nama: ${human.name}")
+    println("tipe: ${human.tipe}")
+    println("hp: ${human.hp}")
+    human.bicara(bicara)
 
 
 }
