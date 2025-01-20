5bb31e3a01ebd55fe8b8df59ca53c961cd2b3a94
chingoo <zaidan.aman@binus.ac.id>
2025-01-20 20:35:21 +0700

    updated

--- src/Main.kt
+++ src/Main.kt
@@ -1,73 +1,45 @@
 fun main(){
-//    println("berapa tiket Reguler yg mau anda beli")
-//    print("1 tiket Rp.50.000>> ")
-//    var tiketReguler = readln().toInt()
-//
-//    println("berapa tiket VIP yg mau anda beli")
-//    print("1 tiket Rp.100.000>> ")
-//    var tiketVIP = readln().toInt()
-//
-//    var hasil = (50000 * tiketReguler)+(100000 * tiketVIP)
-//
-//    println("Total Harga: $hasil")
+    //soal 1
 
-//    var acara = arrayOf("1.tebak animeh", "2.sigma meter", "3.gartic champion", "4.tebak bias")
-//    println("jadwal acara")
-//    for (x in acara){
-//        println(x)
+//    println("masukan nama: ")
+//    var nama = readln().toString()
+//    println("masukan NIM: ")
+//    var nim = readln().toInt()
+//    println("masukan jurusan: ")
+//    var jurusan = readln().toString()
+//
+//    println("nama: $nama")
+//    println("nim: $nim")
+//    println("jurusan: $jurusan")
+
+    //soal 2
+
+//    println("masukan usia:")
+//    var usia = readln().toInt()
+//
+//    if (usia >= 0 && usia <= 12){
+//        println("Anak-anak")
+//    }
+//    else if (usia >= 13 && usia <= 19 ){
+//        println("Remaja")
+//    }
+//    else if (usia >=20 && usia <= 59){
+//        println("Dewasa")
+//    }
+//    else if(usia >= 60){
+//        println("lansia")
 //    }
 
-//    println("apakah anda mahasiswa?")
-//    println("1. Iya")
-//    println("2. Tidak")
-//    var pilih = readln().toInt()
-//
-//    if (pilih == 1){
-//        println("harga total:90000")
-//    }else if (pilih == 2){
-//        println("harga total:100000")
-//    }
-
-
-    var tiketReguler = TiketReguler(50000)
-    tiketReguler.tampilkanHarga()
-
-    var tiketVip = TiketVip(100000)
-    tiketVip.tampilkanHarga()
-
-
-
-
-}
-
-open class Tiket(var harga:Int){
-
-
-    open fun tampilkanHarga(){
-        println("Harga Tiket: Rp $harga")
-
+    var car = arrayOf("volvo", "mustang", "civic", "yamahal")
+    for (x in car){
+        println(x)
     }
-
-}
-
-class TiketReguler(var hargaReguler: Int):Tiket(hargaReguler){
-
-    var fasilitas = "bangku plastik"
-
-    override fun tampilkanHarga(){
-        super.tampilkanHarga()
-        println("fasilitas: $fasilitas")
-
-    }
+    println("mana mobil yang anda pilih?")
+    var pilih = readln().toInt()
+    println("$pilih.${car[pilih-1]}")
 
 
-}
 
-class TiketVip(var hargaVip: Int):Tiket(hargaVip){
-    var fasilitas = "bangku sultan"
+    
 
-    override fun tampilkanHarga() {
-        super.tampilkanHarga()
-        println("fasilitas: $fasilitas")
-    }
 }
