b726733b7461e2a8e22c640e11fc8a14be7c0edf
chingoo <zaidan.aman@binus.ac.id>
2025-01-21 10:18:53 +0700

    updated

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,34 @@
-fun main(){
-    //soal 1
+open class Penduduk(var nama:String, var age:Int){
+    open fun tampilkanData(){
+        println("nama: $nama")
+        println("umur: $age")
+    }
+}
 
-//    println("masukan nama: ")
+class Warga(var namaWarga:String, var ageWarga:Int, var noRT:Int, var noRW:Int):Penduduk(namaWarga, ageWarga){
+
+    fun tampilkanDataWarga(){
+        super.tampilkanData()
+        println("RT: $noRT")
+        println("RW: $noRW")
+
+    }
+}
+
+
+
+
+
+fun main(){
+
+    //soal 1
+//    println("Masukan nama: ")
 //    var nama = readln().toString()
-//    println("masukan NIM: ")
+//
+//    println("Masukan NIM: ")
 //    var nim = readln().toInt()
-//    println("masukan jurusan: ")
+//
+//    println("Masukan Jurusan: ")
 //    var jurusan = readln().toString()
 //
 //    println("nama: $nama")
@@ -13,30 +36,55 @@ fun main(){
 //    println("jurusan: $jurusan")
 
     //soal 2
-
-//    println("masukan usia:")
-//    var usia = readln().toInt()
+//    println("masukan umur")
+//    var age = readln().toInt()
 //
-//    if (usia >= 0 && usia <= 12){
-//        println("Anak-anak")
+//    if (age >= 0 && age <= 12 ){
+//        println("anak-anak")
 //    }
-//    else if (usia >= 13 && usia <= 19 ){
+//    else if(age >= 13 && age <=19){
 //        println("Remaja")
 //    }
-//    else if (usia >=20 && usia <= 59){
-//        println("Dewasa")
+//    else if(age >= 20 && age <= 59){
+//        println("dewasa")
 //    }
-//    else if(usia >= 60){
+//    else if(age >= 60){
 //        println("lansia")
 //    }
 
-    var car = arrayOf("volvo", "mustang", "civic", "yamahal")
-    for (x in car){
-        println(x)
-    }
-    println("mana mobil yang anda pilih?")
-    var pilih = readln().toInt()
-    println("$pilih.${car[pilih-1]}")
+    //soal 3
+//    println("masukan harga: ")
+//    var harga = readln().toInt()
+//    println("masukan diskon: ")
+//    var diskon:Int
+//    do {
+//        diskon = readln().toInt()
+//    }while (diskon <= 0 && diskon >= 101)
+//
+//    var hasil = harga -(harga * diskon/100)
+//
+//    println("total harga setelah diskon: $hasil")
+
+    //soal 4
+
+//    var car = arrayOf("BMW", "Mustang", "Toyota","Volvo", "Ferrari")
+//
+//    for (mobil in car.indices){
+//        println("${mobil+1} ${car[mobil]}")
+//    }
+//    println("Pilih mobil impian Anda")
+//    var pilih = readln().toInt()
+//    println("mobil yang anda pilih "+car[pilih-1])
+
+    //soal 5
+    var saiful = Warga("saiful", 38, 7, 13)
+
+    var joko = Warga("joko", 48, 7, 10)
+
+    saiful.tampilkanDataWarga()
+    println("")
+    joko.tampilkanDataWarga()
+
 
 
 
