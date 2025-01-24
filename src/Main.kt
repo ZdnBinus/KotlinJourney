72e3d5db431e427a7635ded043142af76a6168ef
chingoo <zaidan.aman@binus.ac.id>
2025-01-24 17:07:54 +0700

    updated

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,55 @@
-class User(val username:String, val password:String){
-    init {
-        require(password.length >= 8){"password must be at least 8 char long"}
-        println("user $username has been created")
+class Students(var name:String, var id:Int, var major:String)
+
+class StudentsManagement(){
+    var students: MutableList<Students> = mutableListOf()
+
+    fun addStudent(student: Students){
+        students.add(student)
+        println("student added successfully")
+
+    }
+
+    fun viewStudents(){
+        if(students.isEmpty()){
+            println("no students found")
+        } else {
+            println("list of students:")
+            students.forEachIndexed { index, student->
+                println("${index + 1}. Name: ${student.name} id: ${student.id} major: ${student.major}")
+            }
+        }
     }
 
 }
 
 fun main(){
-    print("masukan username>> ")
-    val username = readln().toString()
-    print("masukan password>> ")
-    val password = readln().toString()
+    var studentsManagement = StudentsManagement()
 
-    var user1 = User(username, password)
+    do {
+        println("\nStudent Management System")
+        println("1. Add Student")
+        println("2. View Students")
+        println("3. Delete Student")
+        println("4. Exit")
+        print("Enter your choice: ")
+        var choice = readln().toInt()
+
+        when (choice){
+            1->{
+                print("nama: ")
+                var nama = readln().toString()
+                print("id: ")
+                var id = readln().toInt()
+                print("major: ")
+                var major = readln().toString()
+
+                studentsManagement.addStudent(Students(nama, id,major))
+            }
+            2->{
+                studentsManagement.viewStudents()
+            }
+            4->break
+        }
+
+    } while (choice != 4)
 }
